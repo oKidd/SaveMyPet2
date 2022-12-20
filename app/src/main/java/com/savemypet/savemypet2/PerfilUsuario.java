b570224cff1d81331cfe1e2649a93dd282b8b5a6
Alejandra <lettar.hyuga>
2022-12-20 01:50:08 -0300

    Se arreglo el prefil de usuario

--- app/src/main/java/com/savemypet/savemypet2/PerfilUsuario.java
+++ app/src/main/java/com/savemypet/savemypet2/PerfilUsuario.java
@@ -1,5 +1,7 @@
 package com.savemypet.savemypet2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,10 +10,21 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.savemypet.savemypet2.clases.Usuario;
 
 public class PerfilUsuario extends AppCompatActivity {
     FirebaseAuth fbAuth;
+    FirebaseUser userAuth;
     TextView tvusername, tvFono, tvEmail;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference refUsuario = database.getReference("user");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +32,27 @@ public class PerfilUsuario extends AppCompatActivity {
         tvusername = findViewById(R.id.profile_name);
         tvFono = findViewById(R.id.profile_phone);
         tvEmail = findViewById(R.id.profile_email2);
-       // FirebaseUser user = fbAuth.getCurrentUser();
+        fbAuth = FirebaseAuth.getInstance();
+        userAuth = fbAuth.getCurrentUser();
+
+
+        refUsuario.child(userAuth.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    tvusername.setText(snapshot.child("name").getValue().toString());
+                    tvEmail.setText(snapshot.child("email").getValue().toString());
+                    tvFono.setText(snapshot.child("fono").getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //tvusername.setText(user.getUid());
        // tvEmail.setText(user.getEmail());
         //tvFono.setText(user.g);
     }
